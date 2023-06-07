def669ce2ee395a15c992b64996037f3cfe19277
Sérgio Rogério <rogerio.bunker@gmail.com>
2023-06-06 22:06:17 -0300

    modificações no java script e nav bar, h1

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
-// script.js
+window.addEventListener('DOMContentLoaded', () => {
+  const questions = document.querySelectorAll('.question');
 
+  questions.forEach(question => {
+    const radioButtons = question.querySelectorAll('input[type="radio"]');
+
+    radioButtons.forEach(button => {
+      button.addEventListener('click', () => {
+        question.classList.add('answered');
+      });
+    });
+  });
+});
